4b9338039277f8df289daaadf4c3f0252754c8d4
L200180063 <anprasetyo060@gmail.com>
2023-10-10 23:48:44 +0700

    fix tampilan

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import { onMounted, ref, watch } from "vue";
 
 const myCart = ref([])
-const addLocalStorage = () => {
-  localStorage.setItem("cart", JSON.stringify(myCart.value));
-}
+// const addLocalStorage = () => {
+//   localStorage.setItem("cart", JSON.stringify(myCart.value));
+// }
 const displayCalc = ref(null)
 const toCalculate = ref(null)
 
@@ -14,29 +14,38 @@ const addToDisplayCalc = (n) => {
     displayCalc.value = String(displayCalc.value) + String(n) :
     displayCalc.value = n
   }
-  if (result.value){
-    result.value = null
-    resetCalc()
-    if(readyToCalc.value){
-      toCalculate.value == displayCalc.value ?
-      [displayCalc.value = null,
-      _addToDisplayCalc(n)] :
-      _addToDisplayCalc(n)
+  const doCalc = (n) => {
+    if (result.value){
+      result.value = null
+      resetCalc()
+      if(readyToCalc.value){
+        toCalculate.value == displayCalc.value ?
+        [displayCalc.value = null,
+        _addToDisplayCalc(n)] :
+        _addToDisplayCalc(n)
+      }
+      else{
+        _addToDisplayCalc(n)
+      }
     }
     else{
-      _addToDisplayCalc(n)
+      if(readyToCalc.value){
+        toCalculate.value == displayCalc.value ?
+        [displayCalc.value = null,
+        _addToDisplayCalc(n)] :
+        _addToDisplayCalc(n)
+      }
+      else{
+        _addToDisplayCalc(n)
+      }
     }
   }
+  if (subTotal.value == 0){
+    doCalc(n)
+  }
   else{
-    if(readyToCalc.value){
-      toCalculate.value == displayCalc.value ?
-      [displayCalc.value = null,
-      _addToDisplayCalc(n)] :
-      _addToDisplayCalc(n)
-    }
-    else{
-      _addToDisplayCalc(n)
-    }
+    isSubTotal.value = false
+    doCalc(n)
   }
 }
 const addToCalculate = () => {
@@ -86,6 +95,7 @@ const getResultCalc = (a) => {
   setTimeout(() => {
     result.value = null
     displayCalc.value = null
+    isSubTotal.value = true
   }, 250)
 }
 const resetCalc = () => {
@@ -95,8 +105,16 @@ const resetCalc = () => {
   operation.value = null
   myPromt.value = null
 }
+const resetAllCalc = () => {
+  subTotal.value = 0
+  myCart.value = []
+  resetCalc()
+  isSubTotal.value = false
+  tempTotal.value = []
+}
 const myPromt = ref(null)
 const subTotal = ref(0)
+const isSubTotal = ref(null)
 const tempTotal = ref([])
 const countTotal = () => {
   let lengthTempTotal = tempTotal.value.length
@@ -108,6 +126,23 @@ const countTotal = () => {
     subTotal.value += tempTotal.value[lengthTempTotal];
   }
 }
+let iniAngka = 1500250123
+const hasilAngka = ref(null)
+const ubahAngka = () => {
+  hasilAngka.value = String(iniAngka).split("")
+  let pjgHasilAngka = (hasilAngka.value.length/3).toFixed(0)
+  for (let a = 0; a<pjgHasilAngka; a++){
+    if (a == 0){
+      hasilAngka.value.splice(-3,0,',')
+    }
+    else{
+      hasilAngka.value.splice(((-3)*(a+1))-a,0,',')
+    }
+  }
+  hasilAngka.value = hasilAngka.value.join('')
+}
+
+const operations = ref(["÷", "x", "-", " +", "="])
 watch(() => {
   if (toCalculate.value){
     readyToCalc.value = true
@@ -125,81 +160,62 @@ onMounted(() => {
 </script>
 
 <template>
-  <main>
-    <section class="container d-flex flex-column justify-content-center align-items-center p-3 vh-100">
-      <div class="">My Cart</div>
-      <div v-if="myCart.length !== 0" class="text-center text-black-50">
-        <ul class="list-unstyled">
-          <li v-for="mc in myCart" :key="mc" class="d-flex gap-2">
-            <div>{{ mc.name }}</div>
-            <div>{{ mc.price + ' ' + mc.operation + ' ' + mc.qty + ' = ' + mc.total}}</div>
-          </li>
-        </ul>
+  <main >
+    <section class="container d-flex flex-column justify-content-center align-items-center p-3 s1 vh-100">
+      <div v-if="myCart.length !== 0">
+        <span v-if="myCart.length == 1">
+          {{ myCart[myCart.length - 1].price + ' ' + myCart[myCart.length - 1].operation + ' ' + myCart[myCart.length - 1].qty }}
+        </span>
+        <span v-else>
+          {{ (subTotal - tempTotal[tempTotal.length - 1]) + ' + (' + myCart[myCart.length - 1].price + ' ' + myCart[myCart.length - 1].operation + ' ' + myCart[myCart.length - 1].qty + ')' }}
+        </span>
       </div>
-      <div v-else class="text-center text-black-50 p-2">Kosong</div>
-      <!-- <button class="btn btn-dark" @click="addLocalStorage()">addLocalStorage</button> -->
-      <div>
-        <span v-if="myPromt">{{ myPromt }}</span>
-        <span v-else style="display: block; min-height: 24px;"></span>
+      <div v-else style="display: block; min-height: 24px;"></div>
+      <div class="p-3 w-100">
+        <div v-if="isSubTotal" class="display-1 w-100 text-center">{{ subTotal }}</div>
+        <input v-else style="display: block; min-height: 53.54px;" type="text" v-model="displayCalc" class="input-my-2 display-1 w-100">
       </div>
-      <input type="text" v-model="displayCalc" :placeholder="displayCalc ? '' : 'Type the price'" class="input-my">
-      <div class="p-3">
-        <div v-if="subTotal" class="fw-bold display-1">{{ subTotal }}</div>
-        <span v-else style="display: block; min-height: 53.54px;"></span>
-      </div>
-      <div class="row w-100">
-        <div class="col-12">
-          <div class="row">
-            <div class="col p-0">
-              <button class="btn border border-1 button-grey btn-round" @click="resetCalc()">
-                <span v-if="!displayCalc" class="fw-bold fs-2">AC</span>
-                <span v-else class="fw-bold fs-2">C</span>
-              </button>
-            </div>
-            <div class="col p-0">
-              <button class="btn border border-1 button-grey btn-round fw-bold fs-1" @click="displayCalc = displayCalc.slice(0, -1)"><i class="bi bi-arrow-left-short"></i></button>
-            </div>
-            <div class="col p-0">
-              <button class="btn border border-1 btn-round fw-bold fs-1" :class="readyToCalc && operation == '%' ? 'btn-dark' : 'button-grey'" @click="addToCalculate(), operation = '%', myPromt = 'Now type the discount'">%</button>
-            </div>
-            <div class="col p-0">
-              <button class="btn border border-1 btn-round fw-bold fs-1" :class="readyToCalc && operation == '/' ? 'btn-dark' : 'button-warning'" @click="addToCalculate(), operation = '/'">&divide;</button>
-            </div>
-          </div>
+      <div class="d-flex flex-column align-items-center justify-content-end s2">
+        <div class="row d-flex w-100 text-center gap-1 p-1 h-100">
+          <button v-if="!displayCalc" @click="resetAllCalc()" class="col d-flex align-items-center justify-content-center button-grey rounded-circle btn fs-3">AC</button>
+          <button v-else @click="resetCalc()" class="col d-flex align-items-center justify-content-center button-grey rounded-circle btn fs-3">C</button>
+          <button class="col d-flex align-items-center justify-content-center button-grey rounded-circle btn fs-1 fw-bold" @click="displayCalc = displayCalc.slice(0, -1)"><i class="bi bi-arrow-left-short"></i></button>
+          <button class="col d-flex align-items-center justify-content-center rounded-circle btn fs-3" @click="addToCalculate(), operation = '%', myPromt = 'Now type the discount'" :class="readyToCalc && operation == '%' ? 'btn-dark' : 'button-grey'">%</button>
+          <button class="col d-flex align-items-center justify-content-center rounded-circle btn fs-1" :class="readyToCalc && operation == '/' ? 'btn -dark' : 'button-warning'" @click="addToCalculate(), operation = '/'">&divide;</button>
         </div>
-        <div class="col-9">
-          <div class="row">
-            <div v-for="n in 9" :key="n" class="col-4 d-flex flex-column justify-content-center align-items-center p-2">
-              <button class="btn button-light border border-1 btn-round fw-bold fs-1" @click="addToDisplayCalc(n)">{{ n }}</button>
-            </div>
-            <div class="col-8 p-2">
-              <button class="btn button-light border border-1 btn-pill fw-bold fs-1" @click="addToDisplayCalc(0)">0</button>
-            </div>
-            <div class="col-4 d-flex flex-column justify-content-center align-items-center p-2">
-              <button class="btn button-light border border-1 btn-round fw-bold fs-1">,</button>
-            </div>
-          </div>
+        <div class="row d-flex w-100 text-center gap-1 p-1 h-100">
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(7)">7</button>
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(8)">8</button>
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(9)">9</button>
+          <button class="col d-flex align-items-center justify-content-center rounded-circle btn fs-3 fs-3" @click="addToCalculate(), operation = '*'" :class="readyToCalc && operation == '*' ? 'btn-dark' : 'button-warning'">x</button>
         </div>
-        <div class="col-3 d-flex flex-column justify-content-center align-items-center row-gap-3">
-          <button class="btn border border-1 btn-round fw-bold fs-1" :class="readyToCalc && operation == '*' ? 'btn-dark' : 'button-warning'" @click="addToCalculate(), operation = '*'">x</button>
-          <button class="btn border border-1 btn-round fw-bold fs-1" :class="readyToCalc && operation == '-' ? 'btn-dark' : 'button-warning'" @click="addToCalculate(), operation = '-'">&minus;</button>
-          <button class="btn border border-1 btn-round fw-bold fs-1" :class="readyToCalc && operation == '+' ? 'btn-dark' : 'button-warning'" @click="addToCalculate(), operation = '+'">+</button>
-          <button class="btn border border-1 btn-round fw-bold fs-2 button-warning" @click="getResultCalc(operation)">Add</button>
+        <div class="row d-flex w-100 text-center gap-1 p-1 h-100">
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(4)">4</button>
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(5)">5</button>
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(6)">6</button>
+          <button class="col d-flex align-items-center justify-content-center rounded-circle btn fs-3" @click="addToCalculate(), operation = '-'" :class="readyToCalc && operation == '-' ? 'btn-dark' : 'button-warning'">&minus;</button>
+        </div>
+        <div class="row d-flex w-100 text-center gap-1 p-1 h-100">
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(1)">1</button>
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(2)">2</button>
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-3" @click="addToDisplayCalc(3)">3</button>
+          <button class="col d-flex align-items-center justify-content-center rounded-circle btn fs-3" @click="addToCalculate(), operation = '+'" :class="readyToCalc && operation == '+' ? 'btn-dark' : 'button-warning'">+</button>
+        </div>
+        <div class="row d-flex w-100 text-center gap-1 p-1 h-100">
+          <button class="col-6 d-flex align-items-center justify-content-center button-light rounded-pill btn fs-3" @click="addToDisplayCalc(0)">0</button>
+          <button class="col d-flex align-items-center justify-content-center button-light rounded-circle btn fs-2">,</button>
+          <button class="col d-flex align-items-center justify-content-center button-warning rounded-circle btn fs-3" @click="getResultCalc(operation)">=</button>
         </div>
       </div>
-      <!-- <div>
-        <div>displayCalc : {{ displayCalc }}</div>
-        <div>operation : {{ operation }}</div>
-        <div>toCalculate : {{ toCalculate }}</div>
-        <div>readyToCalc : {{ readyToCalc }}</div>
-        <div>result : {{ result }}</div>
-      </div> -->
     </section>
 
   </main>
 </template>
 
 <style lang="scss" scoped>
+.s1{
+  min-height: 100vh;
+}
 .input-my{
   outline: none;
   border: none;
@@ -207,6 +223,11 @@ onMounted(() => {
   padding: 1rem;
   text-align: center;
   border-radius: 3rem;
+  &-2{
+    outline: none;
+    border: none;
+    text-align: center;
+  }
 }
 .button{
   &-grey{
@@ -225,12 +246,32 @@ onMounted(() => {
   --btn-gap: 1rem;
   --btn-size: calc((100vw / 4) - var(--btn-gap));
   border-radius: var(--btn-size);
-  height: var(--btn-size);
+  height: 100%;
+  // height: var(--btn-size);
 }
 .btn-round{
-  width: var(--btn-size);
+  width: 100%;
+  // width: var(--btn-size);
 }
 .btn-pill{
   width: 100%;
+}
+.s2{
+  width: 100%;
+  height: 55vh;
+}
+@media (min-width: 576px) { 
+  .s1{
+    --max-w: 50vw;
+    max-width: var(--max-w);
+  }
+  .s2{
+    min-height: 73vh;
+    width: 25vw;
+  }
+  // .btn-round, .btn-pill{
+  //   --btn-gap: 1rem;
+  //   --btn-size: calc((var(--max-w) / 4) - var(--btn-gap));
+  // }
 }
 </style>
